7e3c3ccf8338c139f6f1b20982a918e085717021
Theo <theo@ideine.fr>
2020-06-30 10:59:10 +0200

    Fix auth from client local

--- ClientWCF/Auth_form.cs
+++ ClientWCF/Auth_form.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using ClientWCF.proxy;
 
@@ -13,17 +7,15 @@ namespace ClientWCF
 	public partial class Auth_form : Form
 	{
 		private STC_MSG msg;
-		private IAuth prox;
+		private AuthClient client;
 
 		public Auth_form()
 		{
 			InitializeComponent();
-		}
 
-		private void Auth_Load(object sender, EventArgs e)
-		{
 			this.msg = new STC_MSG();
-			prox = new IAuth.Client();
+			this.client = new AuthClient("authTcp");
+
 		}
 
 		private void Btn_go_Click(object sender, EventArgs e)
@@ -45,13 +37,11 @@ namespace ClientWCF
 				{
 					this.txt_information.Text = "Vous n'êtes pas connectés";
 				}
-
 			}
 			else
 			{
 				this.txt_information.Text = "Veuillez renseigner tous les champs";
 			}
-
 		}
 
 		private void M_auhentifier(STC_MSG msg)
@@ -63,9 +53,7 @@ namespace ClientWCF
 			this.msg.app_version = "2.0";
 			this.msg.op_info = "Demande de service de l'application 1 de version 2.0";
 
-			// send login, password, apptoken
-			// return token user
-			this.msg = this.prox.Login(this.msg);
+			this.msg = this.client.Login(this.msg);
 		}
 	}
 }
